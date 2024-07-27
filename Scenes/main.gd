7aa0a0308eb0ff74d561891703a57a2a52eafc17
Robert Bragg <superbleh100@gmail.com>
2024-07-27 12:03:59 -0400

    Cleaned up folder directory

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
 func _on_go_to_death_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/death/death_screen.tscn")
+	get_tree().change_scene_to_file("res://Scenes/death/deathScreen.tscn")
+	pass # Replace with function body.
+
+
+func _on_open_upgrades_button_pressed():
+	$UpgradeModal.visible=true
 	pass # Replace with function body.
--- Scenes/title/titleScreen.gd
+++ Scenes/title/titleScreen.gd
@@ -6,5 +6,5 @@ func _on_quit_button_pressed():
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/character_selection/character_selection.tscn")
+	get_tree().change_scene_to_file("res://Scenes/characterSelection/characterSelection.tscn")
 	pass # Replace with function body.
